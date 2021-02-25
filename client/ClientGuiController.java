7b8b25e975ffd6e5cfda352fe4452ef850ec8c68
Alexey <69649787+Achichev@users.noreply.github.com>
2021-02-25 19:46:35 +0300

    Update ClientGuiController.java

--- client/ClientGuiController.java
+++ client/ClientGuiController.java
@@ -1,4 +1,4 @@
-package com.javarush.task.task30.task3008.client;
+package chat.client;
 
 public class ClientGuiController extends Client {
     private ClientGuiModel model = new ClientGuiModel();
